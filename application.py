ba0dbd4b4a697ab03731291b01df67e07ebbbde4
Evan <evan@whitecoat.com.au>
2018-10-26 15:08:32 +1100

    change server port

--- application.py
+++ application.py
@@ -53,5 +53,5 @@ def valid_token(token):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 9000))
     app.run(host='0.0.0.0', port=port)
